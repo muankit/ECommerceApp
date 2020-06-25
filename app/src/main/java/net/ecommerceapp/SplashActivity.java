3fe9ff7d8c4adc319b49b4ee3ab89fac94ffd17a
Ankit Pareek <mu.ankit98@gmail.com>
2020-06-25 14:31:51 +0530

    Splash Added and login activity created to work

---
+++ app/src/main/java/net/ecommerceapp/SplashActivity.java
@@ -0,0 +1,78 @@
+package net.ecommerceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        boolean isUserExists = checkUser();
+
+        if (isUserExists) {
+
+            Thread myThread1 = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        sleep(500);
+
+                        // TODO change main activity to another here
+                        Intent loginIntent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(loginIntent);
+                        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                        finish();
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            };
+
+            myThread1.start();
+        }else{
+            Thread myThread2 = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        sleep(500);
+
+                        Intent loginIntent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(loginIntent);
+                        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                        finish();
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            };
+            myThread2.start();
+        }
+    }
+
+    private boolean checkUser() {
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user!= null){
+            return true;
+        }
+
+        return false;
+    }
+}
